2c25b78f426e161354407f3d12b0e628c336fd2b
Marianela Vivar <mvivaraguilera@gmail.com>
2022-06-03 22:46:04 -0400

    funciones boton enviar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,12 @@
 $(function (){
-    //$('[data-bs-toggle="tooltip"]').tooltip()
+    $('[data-bs-toggle="tooltip" ]').tooltip();
+
+    $('#enviarCorreo').click(function(){
+      alert("Mensaje enviado correctamente");
+    });
 });
 
-$(document).scroll(function(){
+    $(document).scroll(function(){
     const y = $("html").scrollTop();
     y > 300 ? $('nav').addClass("nav-black") : $("nav").removeClass("nav-black");
-});
+    });
